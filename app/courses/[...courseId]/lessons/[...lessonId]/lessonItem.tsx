632c590de0f26a331d6be3cecd3e0da8847e9d14
NocerGal <luc.schenherr@gmail.com>
2023-12-13 10:47:02 +0100

    correction d'importation

--- app/courses/[...courseId]/lessons/[...lessonId]/lessonItem.tsx
+++ app/courses/[...courseId]/lessons/[...lessonId]/lessonItem.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@/components/ui/Typography';
 import { Badge } from '@/components/ui/badge';
-import { Lesson } from '@prisma/client';
+
 import { CourseLessonItem } from '../../course.query';
 import { CheckCircle, Circle, CircleDashed } from 'lucide-react';
 import Link from 'next/link';
@@ -24,7 +24,7 @@ export const LessonItem = ({ lesson }: LessonItemProps) => {
   const Icon = getLessonIcon(lesson.progress);
 
   return (
-    <Link href={`/courses/${lesson.courseId}`}>
+    <Link href={`/courses/${lesson.courseId}/lessons/${lesson.id}`}>
       <div className="flex items-center gap-3 rounded border border-border bg-card px-4 py-2 transition-colors hover:bg-accent">
         <Icon size={16} />
         <Typography variant="small" className="flex-1">
